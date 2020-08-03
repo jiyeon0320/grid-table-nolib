067acc861a7abf5c8f3d1cae49f764829bd79852
jiyeon0320 <34732127+jiyeon0320@users.noreply.github.com>
2020-08-04 00:51:42 +0900

    삭제 기능 구현하기

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,61 +1,67 @@
 import React, { useState } from 'react';
 
 //인풋 컴포넌트
-const Input = ({ data, handleSubmit }) => {
-    const [input, setInput] = useState(data);
-    const [edit, setEdit] = useState(false);
-    // console.log(input.study_date);
-    const { study_date, grade, original_id, dailyno } = input;
-    // console.log(input);
+const Input = ({ data, onChangeData, index }) => {
+  // console.log(input.study_date);
+  const { study_date, grade, original_id, dailyno, stat } = data;
+  // console.log(input);
 
-    const onClick = () => {
-        setEdit(!edit);
-        console.log('click');
-    };
-    const onChange = (e) => {
-        console.log(e.target.value);
-        console.log(data.dailyno);
-        setInput(e.target.value);
-    };
-    const onSubmit = (e) => {
-        console.log(e);
-        e.preventDefault();
-        handleSubmit(data.dailyno, input);
-    };
-    return (
-        <>
-            {edit ? (
-                <tr onSubmit={onSubmit}>
-                    <td>
-                        <input value={study_date} onChange={onChange} />
-                    </td>
-                    <td>
-                        <input value={grade} onChange={onChange} />
-                    </td>
-                    <td>
-                        <input value={original_id} onChange={onChange} />
-                    </td>
-                    <td>{dailyno}</td>
-                </tr>
-            ) : (
-                <tr onSubmit={onSubmit}>
-                    <td name={data.study_date} onClick={onClick}>
-                        {study_date}
-                    </td>
-                    <td name={grade} onClick={onClick}>
-                        {grade}
-                    </td>
-                    <td name={original_id} onClick={onClick}>
-                        {original_id}
-                    </td>
-                    <td name={dailyno} onClick={onClick}>
-                        {dailyno}
-                    </td>
-                </tr>
-            )}
-            {/* <button></button> */}
-        </>
+  const onClick = () => {
+    onChangeData(
+      {
+        ...data,
+        stat: 'U',
+      },
+      index
     );
+  };
+
+  const onChange = (name) => (e) => {
+    console.log('save');
+    console.log(e);
+    onChangeData(
+      {
+        ...data,
+        [name]: e.target.value,
+      },
+      index
+    );
+  };
+
+  return (
+    <>
+      {stat === 'U' || stat === 'I' ? (
+        <tr>
+          <td>
+            <input value={study_date} onChange={onChange('study_date')} />
+          </td>
+          <td>
+            <input value={grade} onChange={onChange('grade')} />
+          </td>
+          <td>
+            <input value={original_id} onChange={onChange('original_id')} />
+          </td>
+          <td>{dailyno}</td>
+        </tr>
+      ) : (
+        <tr>
+          <td name={study_date} onClick={onClick}>
+            {study_date}
+          </td>
+          <td name={grade} onClick={onClick}>
+            {grade}
+          </td>
+          <td name={original_id} onClick={onClick}>
+            {original_id}
+          </td>
+          <td name={dailyno} onClick={onClick}>
+            {dailyno}
+          </td>
+        </tr>
+      )}
+      {/* <button></button> */}
+    </>
+  );
 };
 
 export default Input;
--- src/pages/CrudTable.js
+++ src/pages/CrudTable.js
@@ -5,96 +5,92 @@ import { requestUpdateGrid, requestViewGrid } from '../actions';
 import Input from '../components/Input';
 
 const CrudTable = () => {
-    // const viewGrid = useSelector((state) => state.viewGrid); //기존 데이터 출력
-    // const dispatch = useDispatch();
-    const [datas, setDatas] = useState(null); //기존 데이터 출력
-    // console.log(datas); //초기 진입시 데이터 출력
-    const handleSubmit = (dailyno, input) => {
-        console.log('submit');
-        console.log(dailyno);
-        if (datas) {
-            setDatas(
-                datas.map((d) =>
-                    d.dailyno === dailyno
-                        ? {
-                              ...d,
-                              study_date: input.study_date,
-                              grade: input.grade,
-                              original_id: input.original_id,
-                          }
-                        : d
-                )
-            );
-        }
+  // const viewGrid = useSelector((state) => state.viewGrid); //기존 데이터 출력
+  // const dispatch = useDispatch();
+  const [datas, setDatas] = useState([]); //기존 데이터 출력
+  // console.log(datas); //초기 진입시 데이터 출력
+  const onChangeData = (changedData, index) => {
+    console.log('submit');
+    setDatas((prevDatas) => {
+      const datas = [...prevDatas];
+      datas[index] = changedData;
+      return datas;
+    });
+  };
 
-        console.log(dailyno, input);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data: viewGrid } = await axios.post(
+          'http://localhost:6373/table/view-grid'
+        );
+
+        setDatas(viewGrid.data);
+      } catch (error) {
+        console.log(error);
+      }
     };
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const result = await axios.post('http://localhost:6373/table/view-grid');
-                setDatas(result.data.data);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        fetchData();
-    }, []);
-
-    //***** saga로 하고 싶음 ****//
-    // useEffect(()=>{
-    //     dispatchEvent(requestViewGrid());
-    // },[]);
+  //데이터 서버로 보내기
+  const onSubmit = (e) => {
+    console.log(`onSubmit..`);
     console.log(datas);
-    if (datas) {
-        console.log(datas[1]);
-    }
+    console.log(e.target);
+    axios.post('http://localhost:6373/table/update-grid', datas);
 
-    //데이터 저장하기
-    const onSave = (e) => {
-        console.log(`onSave..`);
-        console.log(datas);
-        // axios.post('http://localhost:6373/table/update-grid',{});
+    // dispatch(requestUpdateGrid({ study_date, grade, original_id, stat, dailyno }));
+  };
 
-        // console.log(`divpatch(requestUpdatedGrid(datas)), ${datas}`);
-    };
+  //페이지 첫 진입시 데이터 없을 때는 로딩중
+  // if (!datas) {
+  //     return <h3>로딩..</h3>;
+  // }
 
-    //페이지 첫 진입시 데이터 없을 때는 로딩중
-    // if (!datas) {
-    //     return <h3>로딩..</h3>;
-    // }
-    if (!datas) return null;
+  const onAddData = () => {
+    setDatas((prevDatas) => [
+      {
+        study_date: '',
+        grade: '',
+        original_id: '',
+        stat: 'I',
+      },
+      ...prevDatas,
+    ]);
+  };
 
-    return (
-        <>
-            <h3>테이블 만들기</h3>
-            <table>
-                <thead>
-                    <tr>
-                        <th>등록일자</th>
-                        <th>학년</th>
-                        <th>내용</th>
-                        <th>번호</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {datas.map((d, i) => (
-                        <Input
-                            key={d.dailyno}
-                            data={d}
-                            study_date={d.study_date}
-                            grade={d.grade}
-                            original_id={d.original_id}
-                            dailyno={d.dailyno}
-                            handleSubmit={handleSubmit}
-                        />
-                    ))}
-                </tbody>
-            </table>
-            <button onClick={onSave}>저장</button>
-        </>
-    );
+  return (
+    <>
+      <h3>테이블 만들기</h3>
+      <button onClick={onAddData}>생성</button>
+      <table>
+        <thead>
+          <tr>
+            <th>등록일자</th>
+            <th>학년</th>
+            <th>내용</th>
+            <th>번호</th>
+          </tr>
+        </thead>
+        <tbody>
+          {datas.map((d, i) => (
+            <Input
+              key={d.dailyno}
+              data={d}
+              study_date={d.study_date}
+              grade={d.grade}
+              original_id={d.original_id}
+              dailyno={d.dailyno}
+              index={i}
+              onChangeData={onChangeData}
+            />
+          ))}
+        </tbody>
+      </table>
+      <button onClick={onSubmit}>저장</button>
+    </>
+  );
 };
 
 export default CrudTable;
